d67ccf4d0e65b5d7e95ef482166eceb06a018848
admin <admin@admin.com>
2020-01-14 06:41:09 +0000

    test source

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -16,17 +16,20 @@
 
 package org.springframework.samples.petclinic.system;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class WelcomeController {
 
-	private final Logger log = LoggerFactory.getLogger(this.getClass());
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
 
 	@GetMapping("/")
 	public String welcome() {
-
+        log.debug("-------------------call URL home-------------------");
 		return "welcome";
 	}
 
